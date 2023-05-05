6c457d434303436df3d7ec317d0bf81511baae2e
ray836 <raygrant36@gmail.com>
2023-05-04 20:38:25 -0600

    dont throw

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	// res.send("Hello World!");
-	res.sendStatus(500);
+	res.send("Hello World!");
+	// res.sendStatus(500);
 });
 
 const port = 8080;
