a23951d824773c5826e32c1cdfe18a58b644b40f
ray836 <raygrant36@gmail.com>
2023-05-10 19:42:48 -0600

    add post

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
 	// res.sendStatus(500);
 });
 
+app.post("/", (req, res) => {
+	res.send("Hello World!");
+	// res.sendStatus(500);
+});
+
 const port = 8080;
 app.listen(port, () => {
 	console.log(`Server started at http://localhost:${port}`);
